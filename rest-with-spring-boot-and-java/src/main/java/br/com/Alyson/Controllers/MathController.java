3bec1b598ef98c0db4c656731091c8c6f247d391
AlysonMatheus <alyson2000@live.com>
2025-05-11 13:32:42 -0300

    Definindo a logica

--- rest-with-spring-boot-and-java/src/main/java/br/com/Alyson/Controllers/MathController.java
+++ rest-with-spring-boot-and-java/src/main/java/br/com/Alyson/Controllers/MathController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/math")
 public class MathController {
@@ -11,9 +12,21 @@ public class MathController {
     @RequestMapping("/sum/{numberOne}/{numberTwo}")
     public Double sum(
             @PathVariable("numberOne") String numberOne,
-            @PathVariable("numberTwo") String numberTwo){
+            @PathVariable("numberTwo") String numberTwo)
+            throws Exception {
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new IllegalArgumentException();
+
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
+    }
+
+    private Double convertToDouble(String numberTwo) {
         return 1D;
     }
+
+    private boolean isNumeric(String number) {
+        return true;
+    }
+
     //http://localhost:8080/math/subtraction/3/5
     //http://localhost:8080/math/division/3/5
 }
