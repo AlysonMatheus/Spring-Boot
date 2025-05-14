f83bfd0f69d32111d9ca142841105f61a45cb051
AlysonMatheus <alyson2000@live.com>
2025-05-14 12:33:29 -0300

    Injeção de dependência

--- rest-with-spring-boot-and-java/src/main/java/br/com/Alyson/PersonServices.java
+++ rest-with-spring-boot-and-java/src/main/java/br/com/Alyson/PersonServices.java
@@ -11,7 +11,17 @@ public class PersonServices {
     private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
+
     public Person findByID(String id){
         logger.info("Finding one Person!");
+
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Alyson");
+        person.setLastName("Matheus");
+        person.setAddress("Osvaldo Cruz");
+        person.setGender("Male");
+        return person;
+
     }
 }
