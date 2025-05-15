162e2f383403128ce018d668714b2b9106f35440
AlysonMatheus <alyson2000@live.com>
2025-05-15 06:16:23 -0300

    Retornando Lista de Objetos em JSON

--- rest-with-spring-boot-and-java/src/main/java/br/com/Alyson/PersonServices.java
+++ rest-with-spring-boot-and-java/src/main/java/br/com/Alyson/PersonServices.java
@@ -3,6 +3,8 @@ package br.com.Alyson;
 import br.com.Alyson.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -12,7 +14,20 @@ public class PersonServices {
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
 
 
+    public List<Person>  findAll(){
+        List<Person> persons = new ArrayList<Person>();
+        for (int i = 0; i <8 ; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+
+        }
+        return persons;
+    }
+
+
     public Person findByID(String id){
+
+
         logger.info("Finding one Person!");
 
         Person person = new Person();
@@ -24,4 +39,15 @@ public class PersonServices {
         return person;
 
     }
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("FirstName " + i);
+        person.setLastName("LastName " + i);
+        person.setAddress("Some Address in Brazil");
+        person.setGender("Male");
+        return person;
+
+    }
+
 }
