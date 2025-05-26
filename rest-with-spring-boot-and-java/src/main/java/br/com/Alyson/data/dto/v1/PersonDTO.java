7ff4242d399aad90b56e591a392041c9b6159b1e
AlysonMatheus <alyson2000@live.com>
2025-05-26 08:27:45 -0300

    Ignorando propriedades condicionalmente

--- rest-with-spring-boot-and-java/src/main/java/br/com/Alyson/data/dto/v1/PersonDTO.java
+++ rest-with-spring-boot-and-java/src/main/java/br/com/Alyson/data/dto/v1/PersonDTO.java
@@ -1,13 +1,14 @@
 package br.com.Alyson.data.dto.v1;
 
 
-
 //import com.fasterxml.jackson.annotation.JsonIgnore;
 //import com.fasterxml.jackson.annotation.JsonProperty;
 //import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 import br.com.Alyson.serializer.GenderSerializer;
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 import java.io.Serializable;
@@ -16,23 +17,32 @@ import java.util.Objects;
 
 
 //@JsonPropertyOrder({"id","first_Name","last_Name","address", "gender"})
+@JsonFilter("PersonFilter")
 public class PersonDTO implements Serializable {
     private static final long serialVersionUID = 1l;
 
 
     private Long id;
+
     //@JsonProperty("first_Name")
     private String firstName;
+
     // @JsonProperty("last_Name")
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String lastName;
+
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
+    private String phoneNumber;
 
     @JsonFormat(pattern = "dd/MM/yyyy")
     private Date birthDay;
     private String address;
 
     // @JsonIgnore
-    @JsonSerialize (using = GenderSerializer.class)
+    @JsonSerialize(using = GenderSerializer.class)
     private String gender;
+
+    private String sensitiveData;
 
     public PersonDTO() {
 
@@ -86,16 +96,32 @@ public class PersonDTO implements Serializable {
         this.birthDay = birthDay;
     }
 
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getSensitiveData() {
+        return sensitiveData;
+    }
+
+    public void setSensitiveData(String sensitiveData) {
+        this.sensitiveData = sensitiveData;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         PersonDTO personDTO = (PersonDTO) o;
-        return Objects.equals(getId(), personDTO.getId()) && Objects.equals(getFirstName(), personDTO.getFirstName()) && Objects.equals(getLastName(), personDTO.getLastName()) && Objects.equals(getBirthDay(), personDTO.getBirthDay()) && Objects.equals(getAddress(), personDTO.getAddress()) && Objects.equals(getGender(), personDTO.getGender());
+        return Objects.equals(getId(), personDTO.getId()) && Objects.equals(getFirstName(), personDTO.getFirstName()) && Objects.equals(getLastName(), personDTO.getLastName()) && Objects.equals(getPhoneNumber(), personDTO.getPhoneNumber()) && Objects.equals(getBirthDay(), personDTO.getBirthDay()) && Objects.equals(getAddress(), personDTO.getAddress()) && Objects.equals(getGender(), personDTO.getGender()) && Objects.equals(getSensitiveData(), personDTO.getSensitiveData());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getId(), getFirstName(), getLastName(), getBirthDay(), getAddress(), getGender());
+        return Objects.hash(getId(), getFirstName(), getLastName(), getPhoneNumber(), getBirthDay(), getAddress(), getGender(), getSensitiveData());
     }
 }
