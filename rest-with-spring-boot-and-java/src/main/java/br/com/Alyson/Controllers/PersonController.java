b5e9eef6f72506df4c6bdb1c77fc663ae52f6bf5
AlysonMatheus <alyson2000@live.com>
2025-06-03 12:43:40 -0300

    Customizando a Documentação do Swagger (OpenAPI)

--- rest-with-spring-boot-and-java/src/main/java/br/com/Alyson/Controllers/PersonController.java
+++ rest-with-spring-boot-and-java/src/main/java/br/com/Alyson/Controllers/PersonController.java
@@ -4,6 +4,12 @@ import br.com.Alyson.data.dto.v1.PersonDTO;
 import br.com.Alyson.data.dto.v2.PersonDTOV2;
 import br.com.Alyson.services.PersonServices;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -14,24 +20,44 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/person/v1")
+@Tag(name = "People", description = "Endpoints for Managing People")
 public class PersonController {
 
     @Autowired
     private PersonServices service;
 
-  @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE,
-          MediaType.APPLICATION_XML_VALUE,
-          MediaType.APPLICATION_YAML_VALUE})
+    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE})
+    @Operation(summary = "Find All People",
+            description = "Find All People",
+            tags = {"People"},
+            responses = {
+                    @ApiResponse(description = "Success",
+                            responseCode = "200",
+                            content =
+                                    {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    array = @ArraySchema(schema = @Schema(implementation = PersonDTO.class)))
+                                    }),
+
+                    @ApiResponse(description = "No Content", responseCode = "204", content = @Content),
+                    @ApiResponse(description = "Bau Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "504", content = @Content)
+            }
+
+    )
     public List<PersonDTO> findAll() {
         return service.findAll();
 
     }
 
-     @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE,
-             MediaType.APPLICATION_JSON_VALUE,
-             MediaType.APPLICATION_XML_VALUE,
-             MediaType.APPLICATION_YAML_VALUE,
-             MediaType.APPLICATION_YAML_VALUE})
+    @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_JSON_VALUE,
+            MediaType.APPLICATION_XML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE,
+            MediaType.APPLICATION_YAML_VALUE})
 
     public PersonDTO findById(@PathVariable("id") Long id) {
         var person = service.findById(id);
@@ -46,23 +72,25 @@ public class PersonController {
             MediaType.APPLICATION_YAML_VALUE},
             produces = {MediaType.APPLICATION_JSON_VALUE,
                     MediaType.APPLICATION_JSON_VALUE,
-                    MediaType.APPLICATION_XML_VALUE,MediaType.APPLICATION_YAML_VALUE})
+                    MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_YAML_VALUE})
     public PersonDTO create(@RequestBody PersonDTO person) {
         return service.create(person);
 
     }
-    @PostMapping( value = "/V2",
+
+    @PostMapping(value = "/V2",
             consumes = {MediaType.APPLICATION_JSON_VALUE,
-                    MediaType.APPLICATION_JSON_VALUE,MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE,
                     MediaType.APPLICATION_YAML_VALUE},
             produces = {MediaType.APPLICATION_JSON_VALUE,
                     MediaType.APPLICATION_JSON_VALUE,
                     MediaType.APPLICATION_XML_VALUE,
                     MediaType.APPLICATION_YAML_VALUE})
-    public PersonDTOV2 create(@RequestBody PersonDTOV2  person) {
+    public PersonDTOV2 create(@RequestBody PersonDTOV2 person) {
         return service.createV2(person);
 
     }
+
     @PutMapping(consumes = {MediaType.APPLICATION_JSON_VALUE,
             MediaType.APPLICATION_JSON_VALUE,
             MediaType.APPLICATION_XML_VALUE,
@@ -75,6 +103,7 @@ public class PersonController {
         return service.update(person);
 
     }
+
     @DeleteMapping(value = "/{id}")
 
     public ResponseEntity<?> delete(@PathVariable("id") Long id) {
