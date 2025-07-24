3c32667f53f0467a887d083bc6a5096f742031cd
AlysonMatheus <alyson2000@live.com>
2025-07-23 21:57:15 -0300

    Criando a Interface e o Controller Responsáveis Pelo Upload e Download

--- rest-with-spring-boot-and-java/src/main/java/br/com/Alyson/services/FileStorageService.java
+++ rest-with-spring-boot-and-java/src/main/java/br/com/Alyson/services/FileStorageService.java
@@ -1,7 +1,10 @@
 package br.com.Alyson.services;
 
+import br.com.Alyson.Controllers.FileController;
 import br.com.Alyson.Exception.FileStorageException;
 import br.com.Alyson.config.FileStorageConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
@@ -14,6 +17,9 @@ import java.nio.file.StandardCopyOption;
 
 @Service
 public class FileStorageService {
+
+    private  static final Logger logger = LoggerFactory.getLogger(FileController.class);
+
     //defino a varialvel aonde vamos armezanar os aqruivos
     private final Path fileStorageLocation;
 
@@ -28,9 +34,11 @@ public class FileStorageService {
         this.fileStorageLocation = path;
 
         try {
+            logger.info("Creating Directories");
             // criando o diretorio de armazenamento
             Files.createDirectories(this.fileStorageLocation);
         } catch (Exception e) {
+            logger.error("Could bot create the directory where files will be stored!");
             // se ocorrer alguma falha vai lançar essa exceção
             throw new FileStorageException("Could bot create the directory where files will be stored! ", e);
         }
@@ -40,13 +48,16 @@ public class FileStorageService {
         String filename = StringUtils.cleanPath(file.getOriginalFilename());
         try {
             if (filename.contains("..")) {
+                logger.error("Sorry Filename contains invalid path Sequence" + filename);
                 throw new FileStorageException("Sorry Filename contains invalid path Sequence" + filename);
             }
+            logger.info("Saving file in Disk");
             // vai determinar o path ate onde o arquivo vai ser salvo e o nome do arquivo
             Path targetLocation = this.fileStorageLocation.resolve(filename);
             Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);// copia o nome do arquivo pro sistema,
             // convertendo o arquivo pra uma Inputstream, passando o destino e pr fim verifica se o arquivo ja existe( com o mesmo nome)
         } catch (Exception e) {
+            logger.error("Could not store file" + filename + ". Please try Again");
             throw new FileStorageException("Could not store file" + filename + ". Please try Again", e);
         }
         return filename; // retorna o nome do arquivo
