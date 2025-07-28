9f1cdfa679ca0ad12748e7659cfb0f6baa9ba41d
AlysonMatheus <alyson2000@live.com>
2025-07-27 21:33:44 -0300

    Implementando o Importador de CSV - Transformando Dados em Objetos

--- rest-with-spring-boot-and-java/src/main/java/br/com/Alyson/file/importer/impl/CsvImporter.java
+++ rest-with-spring-boot-and-java/src/main/java/br/com/Alyson/file/importer/impl/CsvImporter.java
@@ -2,13 +2,44 @@ package br.com.Alyson.file.importer.impl;
 
 import br.com.Alyson.data.dto.v1.PersonDTO;
 import br.com.Alyson.file.importer.contract.FileImporter;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.stereotype.Component;
 
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class CsvImporter implements FileImporter {
     @Override
     public List<PersonDTO> importFile(InputStream inputStream) throws Exception {
-        return List.of();
+        CSVFormat format = CSVFormat.Builder.create()
+                .setHeader()
+                .setSkipHeaderRecord(true)// pular o cabeçalho
+                .setIgnoreEmptyLines(true)// ignorar linhas vazias
+                .setTrim(true)// elimar os espaco antes/depois
+                .build();
+
+
+        Iterable<CSVRecord> records = format.parse(new InputStreamReader(inputStream)); // pegando cada linha da planinha e simplificando em uma lista de record
+        return parseRecordsToPersonDTos(records);
+    }
+
+    private List<PersonDTO> parseRecordsToPersonDTos(Iterable<CSVRecord> records) {// criando lista de person
+        List<PersonDTO> people = new ArrayList<>();
+        for ( CSVRecord record: records){
+            PersonDTO person = new PersonDTO();
+            person.setFirstName(record.get("first_name"));
+            person.setLastName(record.get("last_name"));
+            person.setAddress(record.get("address"));
+            person.setGender(record.get("gender"));
+            person.setEnabled(true);
+            people.add(person);
+        }
+
+        return people;
+
     }
 }
